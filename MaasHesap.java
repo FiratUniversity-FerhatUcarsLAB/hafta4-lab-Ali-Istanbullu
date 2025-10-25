17751591d3c0f72f49b8ac238b2b8f4238737ffd
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-25 17:38:19 +0300

    Update MaasHesap.java

--- MaasHesap.java
+++ MaasHesap.java
@@ -1,1 +1,10 @@
-
+/*
+ * Ad Soyad: [ALİ İSTANBULLU]
+ * Ogrenci No: [250542019]
+ * Tarih: [25.10.2025]
+ * Aciklama: Gorev 1 - Maas Hesaplama Sistemi
+ * 
+ * Bu program kullanicidan ogrenci bilgilerini alir ve
+ * duzenli bir formatta ekrana yazdirir.
+ * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
+ */
